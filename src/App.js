8e0415797bc4ccb218f30422938ebac18bca4854
myaduvanshi <mmukesh1508@gmail.com>
2024-01-28 03:35:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from './Components/Hero/Hero';
+import Navbar from './Components/Navbar/Navbar';
+import Parallax from './Components/Parallax/Parallax';
+import './app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <section id='Homepage'>
+      <Navbar/>
+      <Hero/>
+    </section>
+    <section id='Services'><Parallax type="services"/></section>
+    <section>Services</section>
+    <section id='Portfolio'><Parallax type="portfolio"/></section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id='Contact'>Contact</section>
+    </>
   );
 }
 
